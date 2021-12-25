f28c339d97bc357819d71bf7479ebc0100c78815
Adeep Krishna Keelar <eippeekeelar@gmail.com>
2021-12-25 11:17:56 +0530

    Second commit

--- Page Replacement Algorithms/First-In-First-Out.c
+++ Page Replacement Algorithms/First-In-First-Out.c
@@ -1,22 +1,57 @@
+/* This is the First-In-First-Out Page Replacement Algorithm.
+   In this simple implementation, Input takes the Size of the 
+   reference string and the reference string and the number of 
+   frames. Output shows the hit and miss ratio.*/
 #include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#define SIZE 20
+#define SIZE 25
 int main(void) {
-    int *reference_string=(int *)malloc(sizeof(int)*SIZE);
-    int number_of_frames=0,size=0,i=0;
-    char c[5];
-    do {
+    int reference_string[SIZE],Size=0;
+    int number_of_frames=0,miss_count=0,hit_count=0,s;
+    float miss_ratio=0.0, hit_ratio=0.0;
+    printf("Enter the pre-determined size of the Reference String: ");
+    scanf("%d",&Size);
+    printf("Enter the Reference String: ");
+    for(int i=0;i<Size;i++) {
+        printf("Value (%d) - ",i+1);
         scanf("%d",&reference_string[i]);
-        i+=1;
-        printf("Do you want to add more numbers?");
-        scanf("%s",c);
-    }while(!strcmp(c,"yes"));
-    size=i;
-    printf("The size is -- %d\n",size);
-    for(int j=0;j<size;j++) {
-        printf("%d ",reference_string[j]);
     }
-    free(reference_string);
+    printf("Enter the number of frames: ");
+    scanf("%d",&number_of_frames);
+    int temp_values[number_of_frames];
+    for(int i=0;i<number_of_frames;i++) {
+        temp_values[i]=-1;
+    }
+    printf("The reference string - ");
+    for(int i=0;i<Size;i++) {
+        printf("%d ",reference_string[i]);
+    }
+    printf("\nThe page replacing algorithm follows -- \n");
+    for(int i=0;i<Size;i++) {
+        s=0;
+        for(int j=0;j<number_of_frames;j++) {
+            if(reference_string[i]==temp_values[j]) {
+                s++;
+                miss_count--;
+            }
+        }
+        miss_count++;
+        if(miss_count<=number_of_frames && (s==0)) {
+            temp_values[i]=reference_string[i];
+        }
+        else if(s==0) {
+            temp_values[(miss_count-1)%number_of_frames]=reference_string[i];
+        }
+        printf("\n");
+        for(int j=0;j<number_of_frames;j++) {
+            printf("%d\t",temp_values[j]);
+        }
+    }
+    hit_count=Size-miss_count;
+    miss_ratio=(float)((float)miss_count/(float)Size);
+    hit_ratio=(float)((float)hit_count/(float)Size);
+    printf("\nTotal number of Miss Counts - %d",miss_count);
+    printf("\nThe Miss Ratio - (%d / %d) = %.2f",miss_count,Size,miss_ratio);
+    printf("\nTotal number of Hit Counts - %d",hit_count);
+    printf("\nThe Hit Ratio - (%d / %d) = %.2f",hit_count,Size,hit_ratio);
     return 0;
 }
